40af28b81f5ca0c34411cc336c8f9cbd3359b444
Mohammed Athaulla <atha.blr1@gmail.com>
2020-05-02 00:12:51 +0530

    Added Models
    Added Models

---
+++ LibraryManagement1/Data/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement1.Data.Model
+{
+    public class Book
+    {
+        public int BookId{ get; set; }
+
+        [Required, MinLength(3), MaxLength(50)]
+        public string Title { get; set; }
+        public virtual Author Author{ get; set; }
+        public int AuthorId { get; set; }
+
+        public virtual Customer Borrower{ get; set; }
+        public int BorrowerId { get; set; }
+    }
+}
---
+++ LibraryManagement1/Data/Model/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement1.Data.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [Required, MinLength(3), MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
